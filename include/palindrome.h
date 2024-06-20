18c9d28f6125bb2d665f3fb3219ebae38d2c1972
noemie <renou.noemie@hotmail.fr>
2024-06-20 14:24:54 +0200

    adding the palindrome function

---
+++ include/palindrome.h
@@ -0,0 +1,13 @@
+#ifndef PALINDROME_H
+#define PALINDROME_H
+
+/**
+ * @file palindrome.h
+ * @brief Check if a string is a palindrome
+ *
+ * @param str A string
+ * @return True or false
+ */
+bool palindrome(char* str);
+
+#endif //PALINDROME_H
---
+++ src/palindrome.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file palindrome.cpp
+ * @brief Implementation of palindrome function.
+ */
+
+#include "palindrome.h"
+#include <string.h>
+
+bool palindrome(char str[]) {
+    length = strlen(str);
+    for (int i = 0; i < length/2; i++) {
+        if (str[i] != str[length-1-i]
+            return false;
+    }
+    return true;
+}
