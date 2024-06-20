0d053008914655878d9d114f7fb548162f62d75c
noemie <renou.noemie@hotmail.fr>
2024-06-20 14:01:42 +0200

    adding the factorial function

---
+++ include/factorial.h
@@ -0,0 +1,13 @@
+#ifndef FACTORIAL_H
+#define FACTORIAL_H
+
+/**
+ * @file factorial.h
+ * @brief Calculate the factorial of an integer
+ *
+ * @param a An integer
+ * @return The factorial of the integer
+ */
+int factorial(int a);
+
+#endif //FACTORIAL_H
---
+++ src/factorial.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file factorial.cpp
+ * @brief Implementation of factorial function.
+ */
+
+#include "factorial.h"
+
+int factorial(int a) {
+    int fact = 1;
+    for (int i = 1; i <= n; i++)
+        fact = fact * i;
+    return fact;
+}
